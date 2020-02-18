a3ffb17433328ed7ad3f6f4664c1ca2acb464534
Rohith Thammaiah <thammaiahrohith@gmail.com>
2020-02-18 06:45:39 +0530

    :tada: init

---
+++ android/app/src/main/kotlin/com/plantershub/coffee_market_prices/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.plantershub.coffee_market_prices
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
